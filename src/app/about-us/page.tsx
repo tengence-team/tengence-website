a057b5c85b6b9e1e91f5e12610e34740f7a84f0b
www <www@gmail.com>
2026-04-04 15:26:00 +0800

    完善关于我们页面

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,6 +1,8 @@
 import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
-
+import {
+  CTASection,
+} from "@/components/home";
 import Image from "next/image";
 
 const sourceHanSerif = localFont({
@@ -249,6 +251,26 @@ export default function AboutUsPage() {
           </div>
         </div>
       </section>
+
+      <section
+        className={cn(
+          "max-w-360 mx-auto px-10 pb-10",
+          sourceHanSerif.className
+        )}
+      >
+        <h2 className="text-[40px] text-black font-semibold">
+          我们欢迎志同道合的你
+        </h2>
+
+        <div className="text-center text-white h-87.5 bg-[url('/images/cooperate.png')] bg-no-repeat bg-cover flex flex-col items-center justify-center gap-6 mt-9 rounded-[20px]">
+          <p className="text-[32px]">
+            通智云在等待同频实干的人才，共造有价值有温度的 AI 产品。
+          </p>
+          <p className="text-[24px]">加入我们：hr@tengence.com</p>
+        </div>
+      </section>
+
+      <CTASection></CTASection>
     </div>
   );
 }
